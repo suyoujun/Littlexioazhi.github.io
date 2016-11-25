8b492c90622ecd8c020dd0303e1c8b5a532cf328
suxioazhi <3107414475@qq.com>
2016-11-25 15:33:12 +0800

    resume

---
+++ js/01.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/11/13 0013.
+ */
+window.onload=function () {
+    // var muc=document.getElementsByClassName('music');
+    var man=document.querSelector('.man');
+    // window.onload()
+    // console.log(man)
+}
